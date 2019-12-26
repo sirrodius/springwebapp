dd6c2c33ba1555a1b81e22e912821de361dcb0df
mapjesca <marek.pjescak@t-systems.com>
2019-12-26 14:54:45 +0100

    Publisher class added

--- src/main/java/guru/springframework/springwebapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/springwebapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.springwebapp.bootstrap;
 
 import guru.springframework.springwebapp.model.Author;
 import guru.springframework.springwebapp.model.Book;
+import guru.springframework.springwebapp.model.Publisher;
 import guru.springframework.springwebapp.repositories.AuthorRepository;
 import guru.springframework.springwebapp.repositories.BookRepository;
+import guru.springframework.springwebapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     AuthorRepository authorRepository;
     BookRepository bookRepository;
+    PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,9 +30,21 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData() {
 
+        Publisher publisher = new Publisher();
+        publisher.setName("Worx");
+        publisher.setAddress("45 Ave Street, Dallas, Texas");
+
+        Publisher publisher2 = new Publisher();
+        publisher2.setName("Mlade Leta");
+        publisher2.setAddress("12 Ruzova Street, Bratislava, Slovakia");
+
+        publisherRepository.save(publisher);
+        publisherRepository.save(publisher2);
+
+
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -37,11 +53,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE development without EJB", "23444", publisher2);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+
+
     }
 }
---
+++ src/main/java/guru/springframework/springwebapp/repositories/PublisherRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.springwebapp.repositories;
+
+import guru.springframework.springwebapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+}
