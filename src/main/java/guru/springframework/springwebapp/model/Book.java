b667ba848ee94cddcf093a7a864d7844f25bd9be
mapjesca <marek.pjescak@t-systems.com>
2019-12-27 12:35:00 +0100

    Lombok Getter and Setter implemented

--- src/main/java/guru/springframework/springwebapp/model/Book.java
+++ src/main/java/guru/springframework/springwebapp/model/Book.java
@@ -1,10 +1,15 @@
 package guru.springframework.springwebapp.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Setter
+@Getter
 @Entity
 public class Book {
 
@@ -38,46 +43,6 @@ public class Book {
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public Set<Author> getAuthors() {
-        return authors;
-    }
-
-    public void setAuthors(Set<Author> authors) {
-        this.authors = authors;
-    }
-
-    public Publisher getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(Publisher publisher) {
-        this.publisher = publisher;
     }
 
     @Override
--- src/main/java/guru/springframework/springwebapp/model/Publisher.java
+++ src/main/java/guru/springframework/springwebapp/model/Publisher.java
@@ -1,10 +1,12 @@
 package guru.springframework.springwebapp.model;
 
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Setter
+@Getter
 @Entity
 public class Publisher {
 
@@ -14,29 +16,4 @@ public class Publisher {
     private String name;
     private String address;
 
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
